efc49d0eaa85344858d43ae778294428ab246cd6
tenglai <13049322087@163.com>
2018-06-28 11:27:47 +0800

    新增弹出框组件

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -2,17 +2,38 @@
  * 商品列表 组件
  */
 import React,{ PureComponent } from 'react'
-
+// 路由跳转
 import {Link} from 'react-router-dom'
 
-import Styles from './index.less'
+import Styles from './index.less';
+// 弹出层 组件
+import Popup from '../../components/Popup';
 // 购物车 背景图
-import scar from '../../assets/scar.svg'
+import scar from '../../assets/scar.svg';
 
 class ProductList extends PureComponent {
+  constructor(props){
+    super(props)
+    this.state = {
+      hidden:true,
+    }
+  }
+
+  clickShopCar = (index)=>{
+    this.setState({
+      hidden:!this.state.hidden,
+    })
+  }
+
+  closePopu = () =>{
+    this.setState({
+      hidden:!this.state.hidden,
+    })
+  }
+
   render(){
     const {data} = this.props;
-    console.log(data);
+    const content = (<div>44444444</div>);
 
     return (
       // 商品列表
@@ -33,7 +54,7 @@ class ProductList extends PureComponent {
                         <p><em>¥ {item.price}</em></p>
                         <div className={Styles.shop_cart}>
                           <div className={Styles.cap_goods_list__buy_btn}>
-                            <img className={Styles.scimg} src={scar} />
+                            <img onClick={this.clickShopCar.bind(this,index)}  className={Styles.scimg} src={scar} />
                           </div>
                         </div>
                       </div>
@@ -43,6 +64,8 @@ class ProductList extends PureComponent {
               }                
             </ul>
           </div>
+
+          <Popup content={content} onclose={this.closePopu} flag={this.state.hidden} />
       </div>
     )
   }
